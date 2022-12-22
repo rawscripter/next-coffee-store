665397d266654431af625e534f93964e81d8f5f5
Shuvo Goswami <rawscripterx@gmail.com>
2022-12-23 03:43:59 +0600

    inital commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,22 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { Inter } from '@next/font/google'
+const inter = Inter({ subsets: ['latin'] })
+import { Poppins } from '@next/font/google'
+import Header from '../components/layout/Header'
+const roboto = Poppins({
+    weight: '400',
+    subsets: ['latin'],
+})
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+    return <>
+        <div className="isolate bg-white">
+            <Header />
+            <main className={roboto.className}>
+                <Component {...pageProps} />
+            </main>
+        </div>
+    </>
 }
